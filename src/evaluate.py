bf50673ee361b7c88885e21fbb6a84239c3a3336
Nahomie07 <110202735+Nahomie07@users.noreply.github.com>
2025-11-27 00:48:35 +0100

    Update evaluate.py

--- src/evaluate.py
+++ src/evaluate.py
@@ -51,14 +51,11 @@ def generate_transcriptions(processor, model, dataset, adapter_weights_path=None
 
     lines = []
     refs = []
-    for item in tqdm(dataset):
-        audio_path = item["audio_filepath"]
-        import torchaudio
-        wav, sr = torchaudio.load(audio_path)
+   for item in tqdm(dataset):
+        wav, sr = item["waveform"], item["sr"]
         if sr != 16000:
-            wav = torchaudio.transforms.Resample(sr, 16000)(wav)
-        wav = wav.squeeze(0).numpy()
-        inputs = processor.feature_extractor(wav, sampling_rate=16000, return_tensors="pt")
+            wav = torchaudio.transforms.Resample(sr, 16000)(wav.unsqueeze(0)).squeeze(0)
+        inputs = processor.feature_extractor(wav.numpy(), sampling_rate=16000, return_tensors="pt")
         input_features = inputs["input_features"].to(device)
         generated_tokens = model.generate(input_features)
         transcription = processor.tokenizer.batch_decode(generated_tokens, skip_special_tokens=True)[0]
