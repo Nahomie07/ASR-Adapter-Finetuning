1d183a370e439026cc4a941e3368cadb7c397c5e
Nahomie07 <110202735+Nahomie07@users.noreply.github.com>
2025-11-27 10:03:08 +0100

    Update hf_utils.py

--- src/hf_utils.py
+++ src/hf_utils.py
@@ -5,5 +5,5 @@ def load_whisper_model(model_name="openai/whisper-small", device="cuda"):
     processor = WhisperProcessor.from_pretrained(model_name)
     model = WhisperForConditionalGeneration.from_pretrained(model_name)
     model.to(device)
-    )
+    
     return processor, model
