2d29a83c2694369e6d83e50217ec8dee9247e330
Nahomie07 <110202735+Nahomie07@users.noreply.github.com>
2025-11-27 09:47:04 +0100

    Update hf_utils.py

--- src/hf_utils.py
+++ src/hf_utils.py
@@ -5,4 +5,7 @@ def load_whisper_model(model_name="openai/whisper-small", device="cuda"):
     processor = WhisperProcessor.from_pretrained(model_name)
     model = WhisperForConditionalGeneration.from_pretrained(model_name)
     model.to(device)
+    model.config.forced_decoder_ids = processor.get_decoder_prompt_ids(
+    language='kin', task="transcribe"
+    )
     return processor, model
